a4a5dfb0d9b15e08e70c8e2cd438252d634d387c
Tulika Kumari <tulikakashyap1999@gmail.com>
2020-04-11 17:05:03 +0530

    added db queries

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:////Users/tulika/Desktop/flasktest/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/tulika/Desktop/flasktest/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
